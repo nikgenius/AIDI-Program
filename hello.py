b812297d2451498d15fa43e99fb019d337f82027
NikC <32002200+nikgenius@users.noreply.github.com>
2022-09-14 21:17:38 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+print ("Hello AIDI1001")
+print ("Hello Ji")
+
+#2) Create Virtual Environment
+# pip install pipenv
+# pipenv install
+# pipenv shell
+# exit()
+# pipenv shell
+
+#3) Install Flask
+# pipenv install flask
+#
